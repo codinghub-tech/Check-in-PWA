bf0ab7f5b68eb2d09d12c5b0847c2ede877a3004
eswaranadh <eshwarnadh345@gmail.com>
2021-04-02 15:42:18 +0530

    Discount/Checkout Code Initiation

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,13 +3,15 @@ import authReducer from "../../services/Authentication/reducers"
 import homeReducer from "../../services/Home/reducers/index"
 import menuReducer from "../../services/MenuServices/reducers/index"
 import cartReducer from "../../services/Cart/reducers/index"
+import checkoutReducer from "../../services/Checkout/reducers/index"
 
 
 const appReducer = combineReducers({
   authentication: authReducer,
   home: homeReducer,
   menu: menuReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  checkout: checkoutReducer
 })
 
 const rootReducer = (state, action) => {
