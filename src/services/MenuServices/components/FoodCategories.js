dacdb4930f0041cb7416c101cb46a0f0e370f502
Franky1001-web <sumitmittal1801@gmail.com>
2021-05-27 16:05:14 +0530

    added key to avoid warnings

--- src/services/MenuServices/components/FoodCategories.js
+++ src/services/MenuServices/components/FoodCategories.js
@@ -33,7 +33,7 @@ function Items() {
             <div ><ScrollMenu
 
                 data={categories.map(res =>
-                    <div style={{ height: '90px', width: '60px', margin: '20px' }}>
+                    <div key={res.title} style={{ height: '90px', width: '60px', margin: '20px' }}>
                         <img style={{ height: '60px', width: '40px' }} src={res.image} />
                         <div style={{ color: '#6d6d6d' }}>{res.title}</div>
                     </div>
